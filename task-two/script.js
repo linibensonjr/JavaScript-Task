688e4a32e0362584a2de21254fff1ee147c6f1e0
linibensonjr <linibensonjr@gmail.com>
2020-04-04 21:52:50 +0100

    add second JS task

--- task-two/script.js
+++ task-two/script.js
@@ -1,21 +1,54 @@
-data = [
+let data = [
     {
         principal: 2500,
-        time: 1.8,
+        time: 1.8
     },
-
     {
         principal: 1000,
-        time: 5,
+        time: 5
     },
-
     {
         principal: 3000,
-        time: 1,
+        time: 1
     },
-
     {
         principal: 2000,
-        time: 3,
-    },
+        time: 3
+    }
 ]
+
+let interestData = [];
+
+function interestCalculator(dataArr) {
+    for (i=0; i<dataArr.length; i++) {
+      //re-assigning the indices
+        let principal = dataArr[i].principal;
+        let time = dataArr[i].time;
+        let rate;
+        if (principal >= 2500 && (time>1 && time<3)) {
+            rate = 3;
+            interest=(principal*rate*time)/100;
+            //new object to store interest results
+            newObj={principal:principal, time:time, rate:rate, interest:interest};
+
+        } else if (principal >= 2500 && time >= 3) {
+            rate = 4;
+            interest=(principal*rate*time)/100;
+            newObj={principal:principal, time:time, rate:rate, interest:interest};
+
+        } else if (principal < 2500 || time <= 1) {
+            rate = 2;
+            interest=(principal*rate*time)/100;
+            newObj={principal:principal, time:time, rate:rate, interest:interest};
+        } else {
+            rate = 1;
+            interest=(principal*rate*time)/100;
+            newObj={principal:principal, time:time,rate:rate, interest:interest};
+        }
+        interestData.push(newObj);
+    }
+    console.log(interestData);
+    return interestData;
+}
+
+interestCalculator(data);
